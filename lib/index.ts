34617b8e272ace204a1442e047f8e274443ae5fe
intrnl <intrnl28+git@gmail.com>
2020-08-19 11:50:19 +0700

    export all methods

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,10 @@
 export * from './core';
 
+export { default as QueryCacheProvider } from './components/CacheProvider.svelte';
+export { default as QueryConfigProvider } from './components/ConfigProvider.svelte';
 
+export { getQueryCache } from './context';
+
+export { query, infiniteQuery } from './use-query';
+export { createPaginatedQuery } from './use-paginatedquery';
+export { isFetching } from './use-isfetching';
